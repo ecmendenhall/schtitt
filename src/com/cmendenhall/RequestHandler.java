d58f192a1ff5111d3397a02c48e16302cd71f1e9
Connor Mendenhall <ecmendenhall@gmail.com>
2013-06-12 09:37:34 -0500

    Add runnable RequestHandler class

--- src/com/cmendenhall/RequestHandler.java
+++ src/com/cmendenhall/RequestHandler.java
@@ -1,11 +1,93 @@
 package com.cmendenhall;
 
-/**
- * Created with IntelliJ IDEA.
- * User: ecm
- * Date: 6/10/13
- * Time: 2:15 PM
- * To change this template use File | Settings | File Templates.
- */
-public class RequestHandler {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class RequestHandler implements Runnable {
+    private RequestParser requestParser;
+    private FileManager fileManager;
+    private MessageLogger logger;
+    private Request request;
+    private Response response;
+    private DateFormat dateFormat;
+    private WebServerSocket socket;
+
+    public RequestHandler(String rawRequest, WebServerSocket webServerSocket) {
+        socket = webServerSocket;
+        dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z");
+        logger = new MessageLogger();
+        fileManager = new FileManager();
+        requestParser = new RequestParser();
+        request = requestParser.makeRequest(rawRequest);
+        response = new Response();
+    }
+
+    public void run() {
+        logger.log(request);
+        serveResponse();
+    }
+
+    public Request getRequest() {
+        return request;
+    }
+
+    private String getCurrentDate() {
+        Date now = new Date();
+        return dateFormat.format(now);
+    }
+
+    private WebResource getRequestedFile() {
+        return fileManager.getWebResource(request.path());
+    }
+
+    private void addDefaultHeaders(Response response) {
+        response.setHeader("Server", "Schtitt/0.9a");
+        response.setHeader("Date", getCurrentDate());
+        response.setHeader("Pragma", "no-cache");
+    }
+
+    private void addFileData(Response response) {
+        WebResource file = getRequestedFile();
+        response.setHeader("Content-Type", file.mimeType());
+        response.setHeader("Content-Length", file.contentLength());
+        response.setHeader("Content-MD5", file.checkSum());
+        response.body(file.binaryData());
+    }
+
+    public Response constructResponse() {
+        if (fileManager.resourceExists(request.path())) {
+            return constructOKResponse();
+        } else {
+            return constructNotFoundResponse();
+        }
+    }
+
+    public Response constructOKResponse() {
+        response.httpVersion("1.0");
+        response.statusCode("200");
+        addDefaultHeaders(response);
+        addFileData(response);
+        return response;
+    }
+
+    public Response constructNotFoundResponse() {
+        response.httpVersion("1.0");
+        response.statusCode("404");
+        addDefaultHeaders(response);
+        response.body("404: Not found.");
+        return response;
+    }
+
+    private void serveResponse() {
+        constructResponse();
+        try {
+            socket.write(response.toBytes());
+            logger.log(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- test/com/cmendenhall/RequestHandlerTest.java
+++ test/com/cmendenhall/RequestHandlerTest.java
@@ -1,11 +1,91 @@
 package com.cmendenhall;
 
-/**
- * Created with IntelliJ IDEA.
- * User: ecm
- * Date: 6/10/13
- * Time: 2:13 PM
- * To change this template use File | Settings | File Templates.
- */
-public class RequestHandler {
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static com.cmendenhall.Utils.join;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
+
+public class RequestHandlerTest {
+    private MockWebServerSocket socket = new MockWebServerSocket();
+    private RequestHandler requestHandler;
+    private String rawRequest;
+    private String body = "";
+    private Response response;
+    private DateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy");
+
+    private String makeRawRequest() {
+        return join("\r\n", "GET /test/sampledirectory/index.html HTTP/1.0",
+                            "Accept: text/html",
+                            "\r\n");
+    }
+
+    @Before
+    public void setUp() throws IOException {
+        rawRequest = makeRawRequest();
+        requestHandler = new RequestHandler(rawRequest, socket);
+        response = requestHandler.constructResponse();
+    }
+
+    @Test
+    public void requestHandlerShouldParseRawRequests() {
+        Request request = requestHandler.getRequest();
+        assertEquals("GET", request.method());
+        assertEquals("/test/sampledirectory/index.html", request.path());
+        assertEquals("text/html", request.getHeader("Accept").get(0));
+        assertEquals(body, request.body());
+    }
+
+    @Test
+    public void responseShouldHaveHTTPVersion() {
+        assertEquals("HTTP/1.0", response.getHttpVersion());
+        assertEquals("200","" + response.getStatusCode());
+    }
+
+    @Test
+    public void responseShouldHaveDefaultHeaders() {
+        Date now = new Date();
+        String today = dateFormat.format(now);
+
+        assertEquals("Schtitt/0.9a", response.getHeader("Server"));
+        assertEquals("no-cache", response.getHeader("Pragma"));
+        assertTrue(response.getHeader("Date").contains(today));
+    }
+
+    @Test
+    public void responseShouldHaveFileData() throws UnsupportedEncodingException {
+        File requestedFile = new File("test/sampledirectory/index.html");
+
+        FileInputStream byteStream;
+        byte[] expected = new byte[(int) requestedFile.length()];
+        try {
+            byteStream = new FileInputStream(requestedFile);
+            int currentByte;
+            int byteIndex = 0;
+            while((currentByte = byteStream.read()) != -1) {
+                expected[byteIndex] = (byte) currentByte;
+                byteIndex++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals("text/html; charset=UTF-8", response.getHeader("Content-Type"));
+        assertEquals("194", response.getHeader("Content-Length"));
+        assertArrayEquals(expected, response.bodyBytes());
+    }
+
+    @Test
+    public void handlerCanWriteResponseToSocket() throws UnsupportedEncodingException {
+        requestHandler.run();
+        assertArrayEquals(response.toBytes(), socket.writtenBytes);
+    }
+
 }
