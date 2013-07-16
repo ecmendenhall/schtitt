47491b3d9356a404452c339185a334f4fd02adbc
Connor Mendenhall <ecmendenhall@gmail.com>
2013-07-16 13:34:39 -0500

    Add customHeaders method to WebResource

--- src/com/cmendenhall/WebResource.java
+++ src/com/cmendenhall/WebResource.java
@@ -1,5 +1,7 @@
 package com.cmendenhall;
 
+import java.util.HashMap;
+
 public interface WebResource {
 
     public String mimeType();
@@ -8,5 +10,6 @@ public interface WebResource {
     public String checkSum();
     public String stringData();
     public byte[] binaryData();
+    public HashMap<String, String> customHeaders();
 
 }
